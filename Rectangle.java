a4cc9a9bf3836a55298f01d96bef7fd483a79857
armanhamza <armanhamza2323@gmail.com>
2026-09-13 14:19:13 +0500

    Area of Rectangle.

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    public static void main(String args[]) {
+        double length=10;
+        double width=20;
+        double area=length * width;
+        System.out.println("Area of Rectangle=" + area);
+}
+}
+
+
+
