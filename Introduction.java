4480ecc71a0d3ca88f00c27ad136293440c5a927
armanhamza <armanhamza2323@gmail.com>
2026-09-16 23:41:37 +0500

    Short Intro

---
+++ Introduction.java
@@ -0,0 +1,7 @@
+public class Introduction {
+    public static void main(String args[]){
+        System.out.println("M.Arman Hamza");
+        System.out.println("Sp26-bai-034");
+        System.out.println("CGPA = 3.6 ");
+    }
+}
